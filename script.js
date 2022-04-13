5a6e7962453addb73367292e2cb5ca1c0970667b
erikswartzdesign <erik.swartz@gmail.com>
2022-04-13 13:35:15 -0600

    Clean up code

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
 
 // Dark or Light Images
 function imageMode(color) {
@@ -13,43 +15,70 @@ function imageMode(color) {
     image3.src=`img/undraw_conceptual_idea_${color}.svg`;
 }
 
+// Cleaner, toggle dark and light mode styles using ternary (? = true, so do this, if not, : = otherwise do this)
+function toggleDarkLightMode(isDark) {
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun'); 
+    isDark? imageMode(DARK_THEME) : imageMode(LIGHT_THEME);   
+    // isDark? imageMode('dark') : imageMode('light');   
+}
+
+// This is the longer, repeated version of writing this code. Try to avoid this...
 // Dark Mode Style
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    // toggleIcon.children[1].classList.remove('fa-sun');
-    // toggleIcon.children[1].classList.add('fa-moon');
-    // toggleIcon.children[1].classList = ('fa-solid fa-moon');
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
+// function darkMode() {
+//     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+//     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+//     toggleIcon.children[0].textContent = 'Dark Mode';
+//     // toggleIcon.children[1].classList.remove('fa-sun');
+//     // toggleIcon.children[1].classList.add('fa-moon');
+//     // toggleIcon.children[1].classList = ('fa-solid fa-moon');
+//     toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+//     imageMode('dark');
     
-}
+// }
 
-// Light Mode Style
-function lightMode(){
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    // toggleIcon.children[1].classList = 'fa-solid fa-sun';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
+// // Light Mode Style
+// function lightMode(){
+//     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+//     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+//     toggleIcon.children[0].textContent = 'Light Mode';
+//     // toggleIcon.children[1].classList = 'fa-solid fa-sun';
+//     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+//     imageMode('light');
     
 
-}
+// }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+        document.documentElement.setAttribute('data-theme', DARK_THEME);
+        localStorage.setItem('theme', DARK_THEME);
+        // document.documentElement.setAttribute('data-theme', 'dark');
+        // localStorage.setItem('theme', 'dark');
+        toggleDarkLightMode(true);
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        document.documentElement.setAttribute('data-theme', LIGHT_THEME);
+        localStorage.setItem('theme', LIGHT_THEME);
+        // document.documentElement.setAttribute('data-theme', 'light');
+        // localStorage.setItem('theme', 'light');
+        toggleDarkLightMode(false);
     }
 }
 
-
-
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if(currentTheme === DARK_THEME) {
+        // if(currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(true);
+    }
+}
